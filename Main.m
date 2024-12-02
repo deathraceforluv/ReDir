8c88d4817a29a2b1c6f5b677d796f8fdb51f0c43
cold <xhanni@yahoo.com>
2024-12-02 04:28:32 -0800

    Test v2
    Testing for SSL Bypass for allowing Local Hosted Backends

--- Main.m
+++ Main.m
@@ -1,11 +1,10 @@
-// Copyright (c) 2024 Project Nova LLC
-
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"http://localhost:3000"  // Change to your local HTTP server URL
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
+// Custom URL Protocol
 @interface CustomURLProtocol : NSURLProtocol
 @end
 
@@ -28,38 +27,62 @@
 
 - (void)startLoading {
     NSMutableURLRequest *modifiedRequest = [[self request] mutableCopy];
-    
+
     NSString *originalPath = [modifiedRequest.URL path];
     NSString *originalQuery = [modifiedRequest.URL query];
-    
+
     NSString *newBaseURLString = API_URL;
     NSURLComponents *components = [NSURLComponents componentsWithString:newBaseURLString];
-    
+
     components.path = originalPath;
     if (originalQuery) {
         components.query = originalQuery;
     }
-    
+
     [modifiedRequest setURL:components.URL];
-    
-    // Mark the request as handled
     [NSURLProtocol setProperty:@YES forKey:@"Handled" inRequest:modifiedRequest];
-    
-    // Ensure that this request forwarding is done safely on the correct thread (main thread)
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[self client] URLProtocol:self
-          wasRedirectedToRequest:modifiedRequest
-                redirectResponse:nil];
-    });
+
+    // Bypass SSL by allowing the modified request to be sent using a custom NSURLSession
+    [self sendRequest:modifiedRequest];
 }
 
 - (void)stopLoading {
-    // Cleanup code (if needed)
+    // Implement stop loading logic if necessary
+}
+
+- (void)sendRequest:(NSURLRequest *)request {
+    // Create a custom NSURLSession that ignores SSL certificate errors
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    // Create a custom delegate to handle SSL bypass
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:self delegateQueue:nil];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Request failed: %@", error);
+        } else {
+            // Handle response data
+            NSLog(@"Request succeeded: %@", response);
+        }
+    }];
+    [task resume];
+}
+
+// SSL bypass handler for the NSURLSession delegate
+- (void)URLSession:(NSURLSession *)session
+       didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+       completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
+    // Always trust the certificate and ignore SSL errors
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+        NSURLCredential *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+        completionHandler(NSURLSessionAuthChallengeUseCredential, credential);
+    } else {
+        completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+    }
 }
 
 @end
 
-// Register CustomURLProtocol in a proper place, such as in AppDelegate or somewhere appropriate
-- (void)applicationDidFinishLaunching:(UIApplication *)application {
+__attribute__((constructor)) void entry() {
     [NSURLProtocol registerClass:[CustomURLProtocol class]];
 }
